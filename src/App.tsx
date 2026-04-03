3503f8b2246552f270d33e2b8a394951d9937acf
Wynter Celedonia <wynterceledonia@Mac.lan>
2026-04-02 20:21:18 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from '@/components/ui/hero-odyssey'
+
+function App() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
